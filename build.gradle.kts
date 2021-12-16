9025fa9012663366f19e124bdf191a8de5351184
bnechyporenko <bogdannechiporenko@gmail.com>
2021-12-16 10:36:46 +0100

    ENG-6757 Use Nebula release plugin for Gradle plugins

---
+++ build.gradle.kts
@@ -0,0 +1,79 @@
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+buildscript {
+    repositories {
+        maven {
+            url = uri("https://plugins.gradle.org/m2/")
+        }
+    }
+}
+
+plugins {
+    kotlin("jvm") version "1.4.20"
+
+    id("groovy")
+    id("idea")
+    id("maven-publish")
+    id("com.github.hierynomus.license") version "0.11.0"
+    id("nebula.release") version "15.3.1"
+}
+
+group = "com.xebialabs"
+project.defaultTasks = listOf("build")
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+    withSourcesJar()
+    withJavadocJar()
+}
+
+val releasedVersion = "2.0.0-${LocalDateTime.now().format(DateTimeFormatter.ofPattern("Mdd.Hmm"))}"
+project.extra.set("releasedVersion", releasedVersion)
+
+repositories { mavenCentral() }
+
+idea {
+    module {
+        setDownloadJavadoc(true)
+        setDownloadSources(true)
+    }
+}
+
+dependencies {
+    implementation(gradleApi())
+    implementation(localGroovy())
+    implementation("com.typesafe:config:1.2.1")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            url = uri("${project.property("nexusBaseUrl")}/repositories/releases")
+            credentials {
+                username = project.property("nexusUserName").toString()
+                password = project.property("nexusPassword").toString()
+            }
+        }
+    }
+}
+
+tasks {
+    named<Upload>("uploadArchives")
+
+    register<NebulaRelease>("nebulaRelease")
+
+    register("dumpVersion") {
+        doLast {
+            file(buildDir).mkdirs()
+            file("$buildDir/version.dump").writeText("version=${releasedVersion}")
+        }
+    }
+
+}
